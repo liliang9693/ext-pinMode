72de18a4cb267c1ec04e1d0b69342687eaf9f7b2
liliang9693 <liliang_wk@163.com>
2020-07-31 17:14:25 +0800

    0.0.1 done

---
+++ README.md
@@ -0,0 +1,51 @@
+# pinMode 引脚模式设置
+
+
+![](./arduinoC/_images/featured.png)
+
+---------------------------------------------------------
+
+## Table of Contents
+
+* [URL](#url)
+* [Summary](#summary)
+* [Blocks](#blocks)
+* [License](#license)
+* [Supported targets](#Supportedtargets)
+
+## URL
+* Project URL : ```https://github.com/liliang9693/ext-pinMode```
+
+* Tutorial URL : ```https://mindplus.dfrobot.com.cn/extensions-user```
+
+
+
+## Summary
+此扩展可以独立设置引脚模式，同时引脚输入框可以插入变量方便灵活使用。注意与Mind+中主控板自带的引脚控制积木不可混用。
+
+## Blocks
+
+![](./arduinoC/_images/blocks.png)
+
+
+
+## Examples
+
+![](./arduinoC/_images/example.png)
+
+## License
+
+MIT
+
+## Supported targets
+
+MCU                | JavaScript    | Arduino   | MicroPython    | Remarks
+------------------ | :----------: | :----------: | :---------: | -----
+arduino        |             |        √      |             | 
+micro:bit        |             |       √       |             | 
+esp32        |             |        √      |             | 
+
+
+## Release Logs
+* V0.0.1  基础功能完成
+
---
+++ arduinoC/main.ts
@@ -0,0 +1,85 @@
+
+enum SIZE {
+    //% block="29*29"
+    1,
+    //% block="58*58"
+    2
+}
+
+enum LINE {
+    //% block="1"
+    1,
+    //% block="2"
+    2,
+    //% block="3"
+    3,
+    //% block="4"
+    4
+}
+
+enum BTN {
+    //% block="A"
+    A,
+    //% block="B"
+    B,
+    //% block="A+B"
+    AB
+}
+
+
+//% color="#1ca2e9" iconWidth=50 iconHeight=40
+namespace toolbox {
+
+    //% block="set pin [PININPUT] pinMode [PINMODE]" blockType="command"
+    //% PININPUT.shadow="string" PININPUT.defl="2"
+    //% PINMODE.shadow="dropdown" PINMODE.options="PINMODE" 
+    export function pinMode(parameter: any, block: any) {
+        let pin=parameter.PININPUT.code;
+        let mode=parameter.PINMODE.code;
+
+        if(pin.charAt()==`\"`) pin=	pin.substring(1,pin.length-1)
+       // console.log(pin)
+        
+        if(Generator.board === 'microbit'){
+            Generator.addCode(`pinMode1(${pin}, ${mode});`);
+       }else{
+        Generator.addCode(`pinMode(${pin}, ${mode});`);
+       }
+    }
+
+    //% block="read digital pin [PIN1]" blockType="boolean"
+    //% PIN1.shadow="dropdownRound"  PIN1.options="PIN_DigitalRead"
+    export function digitalRead(parameter: any, block: any) {
+        let pin=parameter.PIN1.code;
+        Generator.addCode([`digitalRead1(${pin})`,Generator.ORDER_UNARY_POSTFIX]);
+
+    }
+       
+    //% block="read analog pin [PIN2]" blockType="reporter"
+    //% PIN2.shadow="dropdownRound"  PIN2.options="PIN_AnalogRead"
+    export function analogRead(parameter: any, block: any) {
+        let pin=parameter.PIN2.code;
+        Generator.addCode([`analogRead1(${pin})`,Generator.ORDER_UNARY_POSTFIX]);
+
+    }
+
+    //% block="pwm pin[PIN3] output [PWM]" blockType="command"
+    //% PIN3.shadow="dropdownRound"  PIN3.options="PIN_AnalogWrite"
+    //% PWM.shadow="number"  PWM.defl="0"
+    export function analogWrite(parameter: any, block: any) {
+        let pin=parameter.PIN3.code;
+        let pwm=parameter.PWM.code;
+        Generator.addCode(`analogWrite1(${pin},${pwm});`);
+
+    }
+
+    //% block="digital pin [PIN4] output [VAL]" blockType="command"
+    //% PIN4.shadow="dropdownRound"  PIN4.options="PIN_DigitalWrite"
+    //% VAL.shadow="number"  VAL.defl="0"
+    export function digitalWrite(parameter: any, block: any) {
+        let pin=parameter.PIN4.code;
+        let val=parameter.VAL.code;
+        Generator.addCode(`digitalWrite1(${pin},${val});`);
+
+    }
+}
